1f4d0d1cbc087bf71a5f41d66612a64d420f69bf
Ramiro Gomez <web@ramiro.org>
2011-02-09 01:43:28 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+import gae_utils as gae
+from lib.BeautifulSoup import BeautifulSoup, SoupStrainer
+import urlparse
+import re
+
+class MainHandler(gae.BaseHandler):
+
+    re_test = re.compile('<a href="([^"]+)"', re.S)
+    # regular expressions from https://code.google.com/p/domxsswiki/wiki/FindingDOMXSS
+    re_domxss_sources = re.compile('(location\s*[\[.])|([.\[]\s*["\']?\s*(arguments|dialogArguments|innerHTML|write(ln)?|open(Dialog)?|showModalDialog|cookie|URL|documentURI|baseURI|referrer|name|opener|parent|top|content|self|frames)\W)|(localStorage|sessionStorage|Database)')
+    re_domxss_sinks = re.compile('((src|href|data|location|code|value|action)\s*["\'\]]*\s*\+?\s*=)|((replace|assign|navigate|getResponseHeader|open(Dialog)?|showModalDialog|eval|evaluate|execCommand|execScript|setTimeout|setInterval)\s*["\'\]]*\s*\()')
+
+    def get(self):
+        self.set_template_value('title', 'DOMXSS Scanner')
+        url = self.get_param('url', '', 'url')
+        if url:
+            self.set_template_value('url', url)
+            response = gae.HTTP().request(url)
+            if response:
+                html = response.content
+                self.domxss_scan(html)
+                script_urls = self.get_script_urls(url, html)
+                self.set_template_value('script_urls', script_urls)
+        self.generate('text/html', 'index.html')
+
+    def get_script_urls(self, url, html):
+        script_urls = []
+        scripts = BeautifulSoup(html, parseOnlyThese=SoupStrainer('script'))
+        for tag in scripts:
+            if tag.has_key('src'):
+                script_urls.append(self.get_absolute_url(url, tag['src']))
+        return script_urls
+
+    def get_absolute_url(self, base, url):
+        parsed = urlparse.urlparse(url)
+        if '' == parsed.scheme == parsed.netloc:
+            url = urlparse.urljoin(base, url)
+        return url
+
+    def domxss_scan(self, text):
+        sources = self.get_domxss_sources(text)
+        self.set_template_value('sources', sources)
+        sinks = self.get_domxss_sinks(text)
+        self.set_template_value('sinks', sinks)
+
+    def get_domxss_list(self, text, regex):
+        return [{'pos_start':m.start(),'pos_end':m.end(), 'match':m.group(0)}
+            for m in regex.finditer(text)]
+
+    def get_domxss_sources(self, text):
+        return self.get_domxss_list(text, self.re_domxss_sources)
+
+    def get_domxss_sinks(self, text):
+        return self.get_domxss_list(text, self.re_domxss_sinks)
+
+def main():
+    application = webapp.WSGIApplication([('/', MainHandler)], debug=True)
+    util.run_wsgi_app(application)
+
+if __name__ == '__main__':
+    main()
