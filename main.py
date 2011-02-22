81ff17a0aab8b5c5178bab8e05b07e7f6518103b
Ramiro Gomez <web@ramiro.org>
2011-02-23 00:44:10 +0100

    use django v1.2

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+from google.appengine.dist import use_library
+use_library('django', '1.2')
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from django.utils import simplejson
